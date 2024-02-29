20ba7f70a8d6541d1555222f7b421bc4e426b77c
HoNguyenNhatVy <161008244+HoNguyenNhatVy@users.noreply.github.com>
2024-02-29 20:34:24 +0700

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+# defining random values in a dataframe using pandas and numpy
+df = pd.DataFrame(
+np.random.randn(30, 10),
+columns=('col_no %d' % i for i in range(10)))
+st.dataframe(df)
